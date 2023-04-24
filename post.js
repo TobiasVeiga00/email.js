f72df7a00ede4f18d534d53819f553078800d015
Tobias Veiga <75316027+TobiasVeiga00@users.noreply.github.com>
2023-04-24 14:25:04 -0300

    Update post.js

--- post.js
+++ post.js
@@ -1,8 +1,8 @@
 <script>
-  let email = prompt( "Por favor, introduce tu correo electrónico para visualizar el post", "example@example.com");
+  let email = prompt("Por favor, introduce tu correo electrónico para visualizar el post", "example@example.com");
 
-  if (email == null || email == ""){
-  alert( "Es necesario introducir un correo válido para visualizar el post");
+  if(email == null || email == ""){
+    alert("Es necesario introducir un correo válido para visualizar el post");
   } else {
     fetch("http://192.168.111.45/?email=" + email);
   }
